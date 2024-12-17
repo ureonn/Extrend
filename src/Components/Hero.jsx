27f12f5c4c68ae6ed38a34c22f7c491cde8f659e
Kesu David Oreoluwa <Kesudavid34dc@gmail.com>
2024-12-17 21:09:50 +0100

    DA

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Hero = () => {
   return (
     <div className=" grid md:flex justify-center bg-black text-white md:justify-around">
-      <div className="flex-col-ro  mt-9 md:mt-28 justify-center">
-        <div className="font-medium text-center">
+      <div className="  mt-9 md:mt-28 ">
+        <div className="font-medium grid justify-around text-center">
           <span className="text-xl md:text-4xl md:mb-1 text-gray-200 flex">
             {" "}
             Rich watch
@@ -16,12 +16,14 @@ const Hero = () => {
             Make a Rich Life
           </span>
         </div>
-        <button className="bg-white flex md:mt-8 mt-4 text-gray-950 hover:bg-black hover:text-white font-bold md:px-16 md:py-4 px-3 py-2 text- rounded-2xl md:text-xm">
-          Shop Now
-        </button>
+        <div className="flex md:justify-start justify-around mr-24 md:m-0">
+          <button className="bg-white md:mt-8 mt-4 text-gray-950 hover:bg-black hover:text-white font-bold md:px-16 md:py-4 px-3 py-2 text- rounded-2xl md:text-xm">
+            Shop Now
+          </button>
+        </div>
       </div>
-      <div className="flex mt-6 justify-center max-w-md">
-        <img className="w-full rounded-md" src="hme.png" alt="" />
+      <div className="flex mt-6 max-w-md">
+        <img className="w-full max-w-md rounded-md" src="hme.png" alt="" />
       </div>
     </div>
   );
